31beb67e3efc2c62e09b77fd3070d86b4960efb4
David Thomason <davidthomason00@gmail.com>
2023-01-23 08:00:38 -0800

    pause point

--- Characters/Player/multiplayer_synchronizer.gd
+++ Characters/Player/multiplayer_synchronizer.gd
@@ -8,12 +8,12 @@ extends MultiplayerSynchronizer
 		else:
 			get_parent().position = val
 
-#@export var x_transform:Vector2:
-#	set(val):
-#		if is_multiplayer_authority():
-#			x_transform = val
-#		else:
-#			get_parent().transform.x = val
+@export var x_transform:Vector2:
+	set(val):
+		if is_multiplayer_authority():
+			x_transform = val
+		else:
+			get_parent().transform.x = val
 
 #@export var player_color:Color:
 #	set(val):
--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -88,6 +88,6 @@ func set_look_direction(value):
 	if not value.x:
 		return
 	var new_look_direction = Vector2(value.x, 0)
-#	sync.x_transform = new_look_direction
+	sync.x_transform = new_look_direction
 	body_parts.transform.x = new_look_direction
 
