3cd0014fa438d227194a7356a2e27f45fd090fbd
David Thomason <davidthomason00@gmail.com>
2023-03-06 20:54:22 -0800

    working kill code

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -20,7 +20,7 @@ var is_ghost = false
 var killer_id : int = 1
 var player_id : int
 
-@onready @export var anim_state = $AnimationPlayer
+@onready var anim_state = $AnimationPlayer
 @onready var body_parts = $Body
 @onready var camera : Camera2D = $Camera
 @onready var label_name : Label = $PlayerName
@@ -45,38 +45,41 @@ func set_color(color: Color, body = body_parts, exclude = excluded_from_coloring
 				set_color(color, e, exclude)
 
 func _physics_process(_delta):
-	if sync.is_multiplayer_authority():
+	if not sync.is_multiplayer_authority(): return
 		
-		input_direction = get_input_direction()
-		set_look_direction(look_direction * input_direction)
+	get_special_keys()
+	
+	input_direction = get_input_direction()
+	set_look_direction(look_direction * input_direction)
+	
+	if input_direction != Vector2.ZERO and is_alive:
+		anim_state.play("Walk")
+	if input_direction == Vector2.ZERO and is_alive:
+		anim_state.play("Idle")
+	if not is_alive:
+		anim_state.play("Burn")
+	if not is_alive and is_ghost:
+		anim_state.play("Ghost")
 		
-		if input_direction != Vector2.ZERO and is_alive:
-			anim_state.play("Walk")
-		if input_direction == Vector2.ZERO and is_alive:
-			anim_state.play("Idle")
-		if not is_alive:
-			anim_state.play("Burn")
-		if not is_alive and is_ghost:
-			anim_state.play("Ghost")
-			
-		# Sync for Multi Player
-		global_position += input_direction.normalized()
-		sync.position = global_position
+	# Sync for Multi Player
+	global_position += input_direction.normalized()
+	sync.position = global_position
 
-		# Update velocity
-		velocity = input_direction.normalized() * move_speed
+	# Update velocity
+	velocity = input_direction.normalized() * move_speed
 
-		# Actually move character
-		move_and_slide()
+	# Actually move character
+	move_and_slide()
 	
 
 func get_special_keys():
+	if not sync.is_multiplayer_authority(): return
+	
 	if Input.is_action_pressed("effect_tester"):
-		print("WOOOOOOORK")
-		is_alive = false
-		kill_target.get_node("AnimationPlayer").play("Burn")
+		kill_target.kill_player.rpc()
 
 func get_input_direction():
+	if not sync.is_multiplayer_authority(): return
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
@@ -99,16 +102,20 @@ func show_kill_button(body: CharacterBody2D):
 func _on_kill_zone_body_entered(body):
 	$KillPlayer.visible = show_kill_button(body)
 	
-	if str(body.name).to_int() != killer_id and body.is_multiplayer_authority():
+	if str(body.name).to_int() != killer_id:
 		kill_target = body
-		body.is_alive = false
 		
 func _on_kill_zone_body_exited(body):
 	$KillPlayer.visible = false
 	
 func _on_kill_player_pressed():
-	kill_target.is_alive = false
+	kill_target.kill_player.rpc_id(kill_target.get_multiplayer_authority())
+	
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "Burn":
 		kill_target.is_ghost = true
+
+@rpc('any_peer')
+func kill_player():
+	kill_target.is_alive = false
